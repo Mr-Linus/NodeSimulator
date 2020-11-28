3256cf1ea57f595c5dd671427c5e6d4e65ec673d
mr-linus <mrfunky001@gmail.com>
2020-11-29 00:38:04 +0800

    update sync node func

--- pkg/controllers/node/node_controller.go
+++ pkg/controllers/node/node_controller.go
@@ -17,14 +17,13 @@ package node
 
 import (
 	"context"
+	"fmt"
 	simv1 "github.com/NJUPT-ISL/NodeSimulator/pkg/api/v1"
 	"github.com/NJUPT-ISL/NodeSimulator/pkg/util"
 	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/klog"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -48,124 +47,134 @@ type NodeSimulatorReconciler struct {
 
 func (r *NodeSimulatorReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	var (
-		ctx     = context.Background()
-		nodeSim = &simv1.NodeSimulator{}
-		nodelist = &v1.NodeList{}
-		err     = r.Client.Get(ctx, req.NamespacedName, nodeSim)
+		ctx      = context.Background()
+		nodeSim  = &simv1.NodeSimulator{}
+		nodeList = &v1.NodeList{}
+		err      = r.Client.Get(ctx, req.NamespacedName, nodeSim)
 	)
 
 	if err != nil {
 		if apierrors.IsNotFound(err) {
 			klog.Warningf("NodeSim: %v Not Found. ", req.NamespacedName.String())
-
-			return ctrl.Result{}, nil
 		} else {
 			klog.Errorf("NodeSim: %v Error: %v ", req.NamespacedName.String(), err)
 		}
+		return ctrl.Result{}, nil
+	}
+
+	// Get Node List
+	err = r.Client.List(ctx, nodeList, &client.MatchingLabels{
+		ManageLabelKey:ManageLabelValue,
+		UniqueLabelKey:nodeSim.GetNamespace() + "-" + nodeSim.GetName(),
+	})
+	if err != nil {
+		return ctrl.Result{}, err
 	}
 
 	if nodeSim.GetFinalizers() == nil {
 		finalizers := []string{NodeSimFinalizer}
 		nodeSim.SetFinalizers(finalizers)
-		err := r.Update(ctx,nodeSim)
+		err := r.Update(ctx, nodeSim)
 		if err != nil {
-			klog.Errorf("NodeSim %v, Set Finalizers Error: %v",req.NamespacedName.String(),err)
+			klog.Errorf("NodeSim %v, Set Finalizers Error: %v", req.NamespacedName.String(), err)
 		}
+		// Get NodeSim
 		err = r.Client.Get(ctx, req.NamespacedName, nodeSim)
+		if err != nil {
+			klog.Errorf("Get NodeSim: %v Error: %v",req.String(),err)
+		}
 	}
 
 	if nodeSim.GetDeletionTimestamp() != nil {
 		//TODO: Delete
+		if nodeList.Items != nil && len(nodeList.Items) > 0{
+			for _,node := range nodeList.Items {
+				fmt.Printf("NodeSim %v Ready to Delete: %v",req.String(),node.GetName())
+				if err := r.Client.Delete(ctx,node.DeepCopy());err != nil {
+					klog.Errorf("NodeSim: %v Delete Node: %v Error: %v",req.NamespacedName.String(),node.GetName(),err)
+				}
+			}
+		}
+		nodeSim.SetFinalizers(nil)
+		if err := r.Update(ctx, nodeSim);err != nil{
+			klog.Errorf("")
+		}
 
-		return ctrl.Result{},nil
-	}
-
-	requirement,err := labels.NewRequirement(UniqueLabelKey,selection.Equals,[]string{nodeSim.GetNamespace()+"-"+nodeSim.GetName()})
-	if err != nil {
-		klog.Errorf("NodeSim: %v Create Requirement Error: %v",req.String(),err)
-	}
-
-	nodeSelector := labels.NewSelector()
-	nodeSelector.Add(*requirement)
-	err = r.Client.List(ctx,nodelist, &client.ListOptions{LabelSelector: nodeSelector})
-	if err != nil {
-		return ctrl.Result{},err
+		return ctrl.Result{}, nil
 	}
 
 	// Delete Nodes
-	if nodelist.Items != nil && len(nodelist.Items) > nodeSim.Spec.Number {
-		for i:= nodeSim.Spec.Number;i < len(nodelist.Items);i++ {
-			nodeName := nodeSim.GetNamespace()+"-"+nodeSim.GetName()+strconv.Itoa(i)
+	if nodeList.Items != nil && len(nodeList.Items) > nodeSim.Spec.Number {
+		for i := nodeSim.Spec.Number; i < len(nodeList.Items); i++ {
+			nodeName := nodeSim.GetNamespace() + "-" + nodeSim.GetName() +"-"+ strconv.Itoa(i)
 			node := &v1.Node{}
 			node.SetName(nodeName)
-			if err := r.Client.Delete(ctx,node);err != nil && !apierrors.IsNotFound(err) {
-				klog.Errorf("NodeSim: %v Delete Node: %v Error: %v",req.String(),node,err)
+			if err := r.Client.Delete(ctx, node); err != nil && !apierrors.IsNotFound(err) {
+				klog.Errorf("NodeSim: %v Delete Node: %v Error: %v", req.String(), node, err)
 			}
 		}
 	}
 
-	r.SyncFakeNode(ctx,nodeSim)
+	r.SyncFakeNode(ctx, nodeSim)
 
 	return ctrl.Result{}, nil
 }
 
-
-
-func (r *NodeSimulatorReconciler) SyncFakeNode(ctx context.Context,nodeSim *simv1.NodeSimulator) {
+func (r *NodeSimulatorReconciler) SyncFakeNode(ctx context.Context, nodeSim *simv1.NodeSimulator) {
 	// Filter
 	if nodeSim.Spec.Number <= 0 {
 		return
 	}
 
-	nodeTemplate,err := GenNode(nodeSim)
-	if  err != nil{
+	nodeTemplate, err := GenNode(nodeSim)
+	if err != nil {
 		return
 	}
 
-	nodeList := make([]*v1.Node,0)
+	nodeList := make([]*v1.Node, 0)
 	// Gen NodeList
 	for i := 0; i < nodeSim.Spec.Number; i++ {
 		vnode := nodeTemplate.DeepCopy()
-		vnode.SetName(nodeSim.GetNamespace()+"-"+nodeSim.GetName()+strconv.Itoa(i))
+		vnode.SetName(nodeSim.GetNamespace() + "-" + nodeSim.GetName() + "-"+strconv.Itoa(i))
 		nodeList = append(nodeList, vnode)
 	}
 
-	Do := func(ctx context.Context,node *v1.Node) {
+	Do := func(ctx context.Context, node *v1.Node) {
 		fakeNode := &v1.Node{}
-		err := r.Client.Get(ctx,types.NamespacedName{
-			Name: node.GetName(),
+		err := r.Client.Get(ctx, types.NamespacedName{
+			Name:      node.GetName(),
 			Namespace: node.GetNamespace(),
-		},fakeNode)
-		if err != nil && apierrors.IsNotFound(err){
-			if err := r.Client.Create(ctx,node); err != nil {
-				klog.Errorf("NodeSim: %v/%v Create Node: %v Error: %v ",nodeSim.GetNamespace(),nodeSim.GetName(),node.GetName(),err)
+		}, fakeNode)
+		if err != nil && apierrors.IsNotFound(err) {
+			if err := r.Client.Create(ctx, node); err != nil {
+				klog.Errorf("NodeSim: %v/%v Create Node: %v Error: %v ", nodeSim.GetNamespace(), nodeSim.GetName(), node.GetName(), err)
 			}
-		}else {
-			ops := []util.Ops{
+		} else {
+			specOps := []util.Ops{
 				{
-					Op: "replace",
-					Path: "/spec",
+					Op:    "replace",
+					Path:  "/spec",
 					Value: node.Spec,
 				},
+			}
+			statusOps := []util.Ops{
 				{
-					Op: "replace",
-					Path: "/status",
+					Op:    "replace",
+					Path:  "/status",
 					Value: node.Status,
 				},
 			}
-
-			if err := r.Client.Patch(ctx,node,&util.Patch{PatchOps: ops}); err != nil {
-				klog.Errorf("NodeSim: %v/%v Patch Node: %v Error: %v ",nodeSim.GetNamespace(),nodeSim.GetName(),node.GetName(),err)
+			if err := r.Client.Patch(ctx, node, &util.Patch{PatchOps: specOps}); err != nil {
+				klog.Errorf("NodeSim: %v/%v Patch Node: %v Error: %v ", nodeSim.GetNamespace(), nodeSim.GetName(), node.GetName(), err)
+			}
+			if err := r.Client.Status().Patch(ctx,node,&util.Patch{PatchOps: statusOps}); err != nil {
+				klog.Errorf("NodeSim: %v/%v Patch Node Status: %v Error: %v ", nodeSim.GetNamespace(), nodeSim.GetName(), node.GetName(), err)
 			}
 		}
 	}
 
-	util.ParallelizeSyncNode(ctx,5,nodeList,Do)
+	util.ParallelizeSyncNode(ctx, 5, nodeList, Do)
 }
-
-
-
-
 
 func (r *NodeSimulatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
--- pkg/controllers/node/node_template.go
+++ pkg/controllers/node/node_template.go
@@ -12,6 +12,12 @@ const (
 	ManageLabelKey = "sim.k8s.io/managed"
 	ManageLabelValue = "true"
 	UniqueLabelKey = "sim.k8s.io/id"
+	NodeOS = "linux"
+	NodeArch = "amd64"
+	NodeOSImage = "CentOS Linux 7 (Core)"
+	NodeKernel = "3.10.0.el7.x86_64"
+	NodeKubeletVersion = "v1.19.1"
+	NodeDockerVersion = "docker://18.6.3"
 )
 
 func GenNode(nodesim *simv1.NodeSimulator) (*v1.Node,error){
@@ -20,7 +26,6 @@ func GenNode(nodesim *simv1.NodeSimulator) (*v1.Node,error){
 	labels[UniqueLabelKey] = nodesim.GetNamespace()+"-"+nodesim.GetName()
 	podcidr := make([]string,0)
 	podcidr = append(podcidr,nodesim.Spec.PodCidr)
-
 	cpu,err := resource.ParseQuantity(nodesim.Spec.Cpu)
 	if err != nil {
 		klog.Errorf("NodeSim: %v/%v CPU ParseQuantity Error: %v",nodesim.GetNamespace(),nodesim.GetName(),err)
@@ -32,7 +37,6 @@ func GenNode(nodesim *simv1.NodeSimulator) (*v1.Node,error){
 		klog.Errorf("NodeSim: %v/%v Memory ParseQuantity Error: %v",nodesim.GetNamespace(),nodesim.GetName(),err)
 		return nil, err
 	}
-
 	pods,err := resource.ParseQuantity(nodesim.Spec.PodNumber)
 	if err != nil {
 		klog.Errorf("NodeSim: %v/%v Pods ParseQuantity Error: %v",nodesim.GetNamespace(),nodesim.GetName(),err)
@@ -57,6 +61,15 @@ func GenNode(nodesim *simv1.NodeSimulator) (*v1.Node,error){
 				"cpu": cpu,
 				"memory": memory,
 				"pods": pods,
+			},
+			NodeInfo: v1.NodeSystemInfo{
+				OperatingSystem: NodeOS,
+				Architecture: NodeArch,
+				OSImage: NodeOSImage,
+				KernelVersion: NodeKernel,
+				KubeletVersion: NodeKubeletVersion,
+				KubeProxyVersion: NodeKubeletVersion,
+				ContainerRuntimeVersion: NodeDockerVersion,
 			},
 		},
 	}
